d62e611eca0c72779704027a36ca6602c1e12e29
Govardhana Anirudh <144611060+anirudh-gov@users.noreply.github.com>
2024-10-27 16:00:20 +0530

    Time: 0 ms (100%), Space: 8.8 MB (82.9%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,10 +3,16 @@ public:
     bool isAnagram(string s, string t) {
         if(s.length()!=t.length()) return false;
         
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t) return true;
-
-        return false;
+        int freq[26] = {0}; 
+        for(int i=0; i<s.size(); i++){
+            freq[s[i]-'a']++;
+            freq[t[i]-'a']--;
+        }
+        for(auto c:freq){
+            if(c<0){
+                return false;
+            }
+        }
+        return true;
     }
 };
