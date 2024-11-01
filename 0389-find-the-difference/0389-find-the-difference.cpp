ec8f303f33323124de2491bf3aacb3aa4cd2ddc9
Govardhana Anirudh <144611060+anirudh-gov@users.noreply.github.com>
2024-11-01 14:00:31 +0530

    Time: 0 ms (100%), Space: 8.2 MB (27.17%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        vector<int> v(26, 0);
+        for (char ch : s) {
+            v[ch - 'a']++;
+        }
+        for (char ch : t) {
+            if (v[ch - 'a'] == 0)
+                return ch;
+            v[ch - 'a']--;
+        }
+        return ' ';
+    }
+};
