8e845e7fe357a10cbb44ceb184f719d519903046
Govardhana Anirudh <144611060+anirudh-gov@users.noreply.github.com>
2024-11-03 10:41:29 +0530

    Time: 4 ms (45.13%), Space: 11.6 MB (54.69%) - LeetHub

---
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minLength(string s) {
+        if(s.length()<=1) return 1;
+        stack<char> stk;
+        stk.push(s[0]);
+        int l = s.length();
+        for(int i=1;i<l;i++){
+            if(!stk.empty()){
+                if(stk.top()=='A' && s[i]=='B') stk.pop();
+                else if(stk.top()=='C'&& s[i]=='D') stk.pop();
+                else stk.push(s[i]);
+            }else{
+                stk.push(s[i]);
+            }
+        }
+        return stk.size();
+    }
+};
