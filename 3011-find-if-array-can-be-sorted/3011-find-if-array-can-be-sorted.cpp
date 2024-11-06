a7e07480dee12d3e97615d9065f89b3dfbfbf062
Govardhana Anirudh <144611060+anirudh-gov@users.noreply.github.com>
2024-11-06 19:22:09 +0530

    Time: 4 ms (34.53%), Space: 31.2 MB (63.03%) - LeetHub

--- 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.cpp
+++ 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.cpp
@@ -1,15 +1,5 @@
 class Solution {
 public:
-
-    int countSetBits(int n){
-        int c= 0;
-        while(n>0){
-            if((n&1)==1) c++;
-            n>>=1;
-        }
-        return c;
-    }
-
     bool canSortArray(vector<int>& nums) {
 
         int n=nums.size();
@@ -18,7 +8,7 @@ public:
                 if(nums[j]<=nums[j+1]){
                     continue;
                 }else{
-                    if(countSetBits(nums[j])==countSetBits(nums[j+1]))
+                    if(__builtin_popcount(nums[j])==__builtin_popcount(nums[j+1]))
                         swap(nums[j],nums[j+1]);
                     else
                         return false;
@@ -26,6 +16,5 @@ public:
             }
         }
         return true;
-
     }
 };
