1554e3a1877665efc7fb0a9fee567764578c119e
Govardhana Anirudh <144611060+anirudh-gov@users.noreply.github.com>
2025-05-15 19:50:03 +0530

    Time: 31 ms (42.57%), Space: 64.1 MB (9.24%) - LeetHub

---
+++ 2576-find-the-maximum-number-of-marked-indices/2576-find-the-maximum-number-of-marked-indices.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxNumOfMarkedIndices(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        int i =0,j=(n+1)/2,c=0;
+
+        vector<bool> v(n,false);
+
+        while(i<(n+1)/2&&j<n){
+            if(2*nums[i]<= nums[j]){
+                c+=2;
+                i++;
+                j++;
+            } else
+                j++;
+        }
+        return c;
+    }
+};
